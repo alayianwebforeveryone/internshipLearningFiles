0ea066ecbda839df3bb5859774c5528ee8ba7cf2
unknown <alayianwebforeveryone>
2023-12-08 03:47:14 -0800

    day 3

--- index.js
+++ index.js
@@ -188,7 +188,7 @@
 // let str = "Hello, world!";
 // let patt = /world/;
 // let result = str.search(patt);
-// console.log(result); 
+// console.log(result); //7 
 
 // ======================replace=================
 
@@ -226,24 +226,172 @@
 
 
 // ==================================Math and numbers=================
-let number = 100000;
+// let number = 100000;
 
 
 // console.log(number.toFixed(2));
 // console.log(number.toPrecision(2));
 // console.log(number.toLocaleString('en-IN'));
 
-let anotherNumber = 234.678;
+// let anotherNumber = 234.678;
 
-console.log(Math.abs(anotherNumber));
-console.log(Math.floor(anotherNumber));
-console.log(Math.ceil(anotherNumber));
-console.log(Math.round(anotherNumber));
-
-
+// console.log(Math.abs(anotherNumber));
+// console.log(Math.floor(anotherNumber));
+// console.log(Math.ceil(anotherNumber));
+// console.log(Math.round(anotherNumber));
 
 
 
 
 
 
+// ===========================Array===================
+//  let anyarr = [1,2 , 3]
+//  let anyarr2 = [1, "4", 3] // this is another declaration
+//  let anyarr3 = [1, "4", 3, [4,6]]// tthis is mixed array with number values and other arrays
+
+//  console.log(anyarr2[1]);
+//  console.log(anyarr3[3][1]);
+ // ========================at()========
+ // let arr = [3,0,3,9,5,7];
+ // let index = 4;
+ // console.log(arr.at(3));
+ // ========================concat()========
+ 
+ // let arr1 = [2,3]
+ // let arr2 = [5,4,4,5]
+ // let arr3 = [5,4,3,5]
+ 
+ // let concatmethod = arr1.concat(arr2);// this is one method
+ // console.log(concatmethod);
+ // console.log(typeof(concatmethod));
+ // console.log(typeof(arr1));
+ 
+ // console.log(arr3.concat(arr1,arr2))
+ 
+ // ========================iterator()========
+//  let myarray = [2, 4, 5, 5,6,7,8];
+
+// let newarr = myarray[Symbol.iterator]();
+
+//  let result =  (newarr.next());
+//  while(!result.done){
+//      console.log(result.value);
+//       result =  (newarr.next());
+   
+//  }
+
+//  =====================pOP=================
+// let anyarray = ["some", "item", "here", "is"]
+// console.log(anyarray.pop()); //is
+// console.log(anyarray);// [ 'some', 'item', 'here' ]
+
+
+//  console.log(result);
+// ================shift============
+// let anyarray = ["some", "item", "here", "is"]
+// console.log(anyarray.shift()); //some
+// console.log(anyarray);// [ 'item', 'here', 'is' ]
+
+// pop and shift are the vicevera of each others
+// ==============push==============
+
+// const animals = ['pigs', 'goats', 'sheep'];
+
+// const count = animals.push('cows');
+// console.log(count);
+// console.log(animals);//[ 'pigs', 'goats', 'sheep', 'cows' ]
+// ================reduce======================
+// let array1 = [3,4,5,6,7];
+// let initialValue = 0;
+// let reducemethod = anyarray.reduce((accumalter, currentvalue) =>
+//   accumalter + currentvalue, initialValue
+
+
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//  (accumulator, currentValue)=>  accumulator + currentValue,
+//   initialValue,
+//   );
+
+// console.log(sumWithInitial);
+
+// ======================recerse====================
+// const array1 = ['one', 'two', 'three'];
+// console.log('array1:', array1);
+// Expected output: "array1:" Array ["one", "two", "three"]
+
+// const reversed = array1.reverse();
+// console.log('reversed:', reversed);
+// Expected output: "reversed:" Array ["three", "two", "one"]
+
+// Careful: reverse is destructive -- it changes the original array.
+// console.log('array1:', array1);
+// Expected output: "array1:" Array ["three", "two", "one"]
+
+// ======================slice====================
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// console.log(animals.slice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+
+// console.log(animals.slice(2, 4));
+// Expected output: Array ["camel", "duck"]
+
+// console.log(animals.slice(1, 5));
+// ==================spread=========================
+// let anyarray = [1,2, 4, 5, 5];
+// let anyarray1 = [3,4, 6, 577, 5];
+
+//    let newarray = [...anyarray, ...anyarray1];
+//  console.log(newarray);
+// =============================objects===============
+// let anysym = Symbol("an") 
+// let myObj = {name: "ali", age: 24, username: "alayiansaeed",
+// [anysym] :"key2"};
+// console.log(myObj.age);
+// console.log(myObj[anysym]);
+// console.log(myObj["name"]);
+// console.log(myObj.age);// change any value of object.
+
+
+// Object.freeze(myObj)//  freez the value of object
+// myObj.age = 34;
+// console.log(myObj.age);// change will not propogate in  value of object.
+
+
+// const myobj = new Object();  // non- singelton object
+
+// myobj.id = "djfjkd";
+// console.log(myobj);
+   
+// const nestedObj =    // it is a nested object
+// {Name: "ali", Fullname: {
+//     firstName: "alayian",
+//     lastName: "saeed"}
+// }
+
+// console.log(nestedObj.Fullname.lastName)
+
+
+// ==================spread to combine multiple object=============
+// let anyobj1 = {a:1,b: 2}
+// let anyobj2 = {c:1,b: 2}
+// let anyobj3 = {e:1,d: 2}
+
+// let spreadobj = {...anyobj1, ...anyobj2, ...anyobj3};
+// console.log(spreadobj);
+
+// ============array of object=========
+
+// let anyarray = [
+//     {a:1, gamil: "skdjfs@gamil.com"},
+//     {b:1, gamil: "skdjfs@gamil.com"},
+// ]
+
+// console.log(anyarray[0].gamil)
+
+// ======print keys of object in an array========
+
+// let anyobj= {a:1, gamil: "skdjfs@gamil.com"};
+// let newarr = console.log(Object.keys(anyobj))
